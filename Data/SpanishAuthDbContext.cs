21dbd98f9ded45e068d2175ceefb35d90a253323
laszl <laszlosupihunor@yahoo.com>
2025-01-14 03:33:32 +0200

    seeding auth db context
    Signed-off-by: laszl <laszlosupihunor@yahoo.com>

--- Data/SpanishAuthDbContext.cs
+++ Data/SpanishAuthDbContext.cs
@@ -1,21 +1,32 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace BlazorSpanyol.Data;
 
-public class SpanishAuthDbContext : IdentityDbContext<IdentityUser>
+// Custom user class with FirstName and LastName
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+public class SpanishAuthDbContext : IdentityDbContext<ApplicationUser>
 {
     public SpanishAuthDbContext(DbContextOptions<SpanishAuthDbContext> options) : base(options)
     {
     }
-    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+    }
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        //seeding roles USER ADMIN MANAGER
 
-
+        // Seeding roles
         var adminRoleId = "f07f938d-6ad3-4ed9-1179-08da6959dddd";
         var managerRoleId = "d4e5f678-90ab-cdef-1234-567890abcd12";
         var userRoleId = "b2c3d4e5-f678-90ab-cdef-1234567890ab";
@@ -25,44 +36,46 @@ public class SpanishAuthDbContext : IdentityDbContext<IdentityUser>
             new IdentityRole
             {
                 Name = "Admin",
-                NormalizedName = "Admin",
+                NormalizedName = "ADMIN",
                 Id = adminRoleId,
                 ConcurrencyStamp = adminRoleId
             },
             new IdentityRole
             {
                 Name = "Manager",
-                NormalizedName = "Manager",
+                NormalizedName = "MANAGER",
                 Id = managerRoleId,
                 ConcurrencyStamp = managerRoleId
             },
             new IdentityRole
             {
                 Name = "User",
-                NormalizedName = "User",
+                NormalizedName = "USER",
                 Id = userRoleId,
                 ConcurrencyStamp = userRoleId
             }
         };
 
         builder.Entity<IdentityRole>().HasData(roles);
-        //seeding ADMINUSER
+
+        // Seeding admin user
         var adminId = "e5f67890-abcd-ef12-3456-7890abcdef12";
-        var adminUser = new IdentityUser
+        var adminUser = new ApplicationUser
         {
             UserName = "admin@spanyolmvc.com",
             Email = "admin@spanyolmvc.com",
-            NormalizedEmail = "admin@spanyolmvc.com".ToUpper(),
-            NormalizedUserName = "admin@spanyolmvc.com".ToUpper(),
-            Id = adminId
+            NormalizedEmail = "ADMIN@SPANYOLMVC.COM",
+            NormalizedUserName = "ADMIN@SPANYOLMVC.COM",
+            Id = adminId,
+            FirstName = "System",
+            LastName = "Administrator"
         };
-        adminUser.PasswordHash = new PasswordHasher<IdentityUser>()
+        adminUser.PasswordHash = new PasswordHasher<ApplicationUser>()
             .HashPassword(adminUser, "12345");
 
-        builder.Entity<IdentityUser>().HasData(adminUser);
+        builder.Entity<ApplicationUser>().HasData(adminUser);
 
-
-        //add all roles to superadmin
+        // Assign all roles to the admin user
         var superAdminRoles = new List<IdentityUserRole<string>>
         {
             new IdentityUserRole<string>
@@ -81,7 +94,7 @@ public class SpanishAuthDbContext : IdentityDbContext<IdentityUser>
                 UserId = adminId
             }
         };
-        builder.Entity<IdentityUserRole<string>>().HasData(superAdminRoles);
 
+        builder.Entity<IdentityUserRole<string>>().HasData(superAdminRoles);
     }
 }
