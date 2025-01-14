f9fec852e0d2ada293896c0d7dd51e982121cf41
laszl <laszlosupihunor@yahoo.com>
2025-01-14 22:56:17 +0200

    added last name firts nem to registration
    Signed-off-by: laszl <laszlosupihunor@yahoo.com>

--- Data/SpanishAuthDbContext.cs
+++ Data/SpanishAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
@@ -8,7 +9,9 @@ namespace BlazorSpanyol.Data;
 // Custom user class with FirstName and LastName
 public class ApplicationUser : IdentityUser
 {
+    [Required]
     public string FirstName { get; set; }
+    [Required]
     public string LastName { get; set; }
 }
 
